02dece9acc4a904d93903098f6a835406120706b
usuarioJapa <willian.hsimabukuro@gmail.com>
2019-02-15 18:13:46 -0200

    init

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <img ref="img" src="./assets/logo.png">
+    <img ref="preview">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Cropper from 'cropperjs'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted () {
+    const image = this.$refs['img']
+    const preview = this.$refs['preview']
+    const cropper = new Cropper(image, {
+      viewMode: 2,
+      dragMode: 'move',
+      preview: preview,
+      responsive: true,
+      modal: true,
+      center: true,
+      crop(event) {
+        console.log(event.detail.x);
+        console.log(event.detail.y);
+        console.log(event.detail.width);
+        console.log(event.detail.height);
+        console.log(event.detail.rotate);
+        console.log(event.detail.scaleX);
+        console.log(event.detail.scaleY);
+      }
+    })
+  }
+}
+</script>
+
+<style lang="scss">
+@import './../node_modules/cropperjs/dist/cropper.css';
+
+#app {
+  img {
+    max-width: 100%;
+  }
+}
+</style>
